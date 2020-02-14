6f35b810911f513d4bd42c914dd47065209b9644
Cüneyt Yıldız <cuneytyildz@gmail.com>
2020-02-14 21:51:30 +0300

    Initial commit

---
+++ face_det.py
@@ -0,0 +1,17 @@
+import cv2
+import  imageio
+
+face_cascade = cv2.CascadeClassifier("haarcascade-frontalface-default.xml")
+eye_cascade = cv2.CascadeClassifier("haarcascade-eye.xml")
+
+def detect(frame):
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray,1.3,5)
+    for(x,y,w,h) in faces:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+
+
+
+
+
+
